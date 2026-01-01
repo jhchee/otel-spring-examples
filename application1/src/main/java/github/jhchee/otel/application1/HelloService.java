bb7d8f7988f7c544aa834bd5fcf5e78becac6ab8
jhchee <cheehong05090@gmail.com>
2026-01-01 14:54:55 +0000

    add Kafka configuration, update Docker Compose, and integrate Micrometer for metrics

--- application1/src/main/java/github/jhchee/otel/application1/HelloService.java
+++ application1/src/main/java/github/jhchee/otel/application1/HelloService.java
@@ -1,8 +1,10 @@
 package github.jhchee.otel.application1;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import io.opentelemetry.api.baggage.Baggage;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.instrumentation.annotations.WithSpan;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -10,20 +12,18 @@ import java.util.Random;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class HelloService {
+    private final MeterRegistry meterRegistry;
 
     @WithSpan
     public void sleep() throws Exception {
-        var parentSpan = Span.current();
+        meterRegistry.counter("hello_service_sleep_called_total").increment();
         var sleepTime = new Random().nextInt(1000);
         Thread.sleep(sleepTime);
         Span.current().setAttribute("sleepTime", sleepTime);
 
         log.info("helloName in baggage 1 = {}", Baggage.current().getEntryValue("helloName"));
-        throwException();
     }
 
-    public void throwException() {
-        throw new RuntimeException("Test exception");
-    }
 }
--- application1/src/main/java/github/jhchee/otel/application1/configuration/OpenTelemetryConfig.java
+++ application1/src/main/java/github/jhchee/otel/application1/configuration/OpenTelemetryConfig.java
@@ -1,71 +1,71 @@
-package github.jhchee.otel.application1.configuration;
-
-import io.opentelemetry.api.common.AttributeKey;
-import io.opentelemetry.api.trace.SpanKind;
-import io.opentelemetry.contrib.sampler.RuleBasedRoutingSampler;
-import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporter;
-import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizerProvider;
-import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
-import io.opentelemetry.sdk.resources.Resource;
-import io.opentelemetry.sdk.trace.export.SpanExporter;
-import java.util.Collections;
-import java.util.Map;
-
-import io.opentelemetry.sdk.trace.samplers.Sampler;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class OpenTelemetryConfig {
-
-  @Bean
-  public AutoConfigurationCustomizerProvider otelCustomizer() {
-    return p ->
-            p.addSpanExporterCustomizer(this::configureSpanExporter)
-                    .addSamplerCustomizer(this::configureSampler)
-                    .addResourceCustomizer(this::configureResource);
-  }
-
-  private Resource configureResource(Resource resource, ConfigProperties configProperties) {
-    return resource
-            .toBuilder()
-            .putAll(resource.getAttributes())
-            .build();
-  }
-
-  private RuleBasedRoutingSampler configureSampler(Sampler fallback, ConfigProperties config) {
-    return RuleBasedRoutingSampler.builder(SpanKind.SERVER, fallback)
-        .drop(AttributeKey.stringKey("helloName"), "Vincent")
-        .build();
-  }
-
-//  @Bean
-//  public OpenTelemetry openTelemetry() {
-//    return GlobalOpenTelemetry.get();
-//  }
+//package github.jhchee.otel.application1.configuration;
+//
+//import io.opentelemetry.api.common.AttributeKey;
+//import io.opentelemetry.api.trace.SpanKind;
+//import io.opentelemetry.contrib.sampler.RuleBasedRoutingSampler;
+//import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporter;
+//import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizerProvider;
+//import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
+//import io.opentelemetry.sdk.resources.Resource;
+//import io.opentelemetry.sdk.trace.export.SpanExporter;
+//import java.util.Collections;
+//import java.util.Map;
+//
+//import io.opentelemetry.sdk.trace.samplers.Sampler;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class OpenTelemetryConfig {
 //
 //  @Bean
-//  public Tracer tracer(OpenTelemetry openTelemetry) {
-//    return openTelemetry.getTracer("");
+//  public AutoConfigurationCustomizerProvider otelCustomizer() {
+//    return p ->
+//            p.addSpanExporterCustomizer(this::configureSpanExporter)
+//                    .addSamplerCustomizer(this::configureSampler)
+//                    .addResourceCustomizer(this::configureResource);
 //  }
-
-  /**
-   * Configuration for the OTLP exporter. This configuration will replace the default OTLP exporter,
-   * and will add a custom header to the requests.
-   */
-  private SpanExporter configureSpanExporter(SpanExporter exporter, ConfigProperties config) {
-    if (exporter instanceof OtlpHttpSpanExporter) {
-      return ((OtlpHttpSpanExporter) exporter).toBuilder().setHeaders(this::headers).build();
-    }
-    return exporter;
-  }
-
-  private Map<String, String> headers() {
-    return Collections.singletonMap("Authorization", "Bearer " + refreshToken());
-  }
-
-  private String refreshToken() {
-    // e.g. read the token from a kubernetes secret
-    return "token";
-  }
-}
+//
+//  private Resource configureResource(Resource resource, ConfigProperties configProperties) {
+//    return resource
+//            .toBuilder()
+//            .putAll(resource.getAttributes())
+//            .build();
+//  }
+//
+//  private RuleBasedRoutingSampler configureSampler(Sampler fallback, ConfigProperties config) {
+//    return RuleBasedRoutingSampler.builder(SpanKind.SERVER, fallback)
+//        .drop(AttributeKey.stringKey("helloName"), "Vincent")
+//        .build();
+//  }
+//
+////  @Bean
+////  public OpenTelemetry openTelemetry() {
+////    return GlobalOpenTelemetry.get();
+////  }
+////
+////  @Bean
+////  public Tracer tracer(OpenTelemetry openTelemetry) {
+////    return openTelemetry.getTracer("");
+////  }
+//
+//  /**
+//   * Configuration for the OTLP exporter. This configuration will replace the default OTLP exporter,
+//   * and will add a custom header to the requests.
+//   */
+//  private SpanExporter configureSpanExporter(SpanExporter exporter, ConfigProperties config) {
+//    if (exporter instanceof OtlpHttpSpanExporter) {
+//      return ((OtlpHttpSpanExporter) exporter).toBuilder().setHeaders(this::headers).build();
+//    }
+//    return exporter;
+//  }
+//
+//  private Map<String, String> headers() {
+//    return Collections.singletonMap("Authorization", "Bearer " + refreshToken());
+//  }
+//
+//  private String refreshToken() {
+//    // e.g. read the token from a kubernetes secret
+//    return "token";
+//  }
+//}
